afcf7995b8f15be604d5122f3acb5c661a6ca548
kienth <45379893+kienth@users.noreply.github.com>
2022-03-17 20:07:35 +0800

    link

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import Navbar from './partial/Navbar';
 import UpperNavbar from './partial/UpperNavbar';
 import Slider from './home/Slider';
-import Footer from './partial/Footer'
+import Footer from './partial/Footer';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Contact from './contact/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <UpperNavbar />
-      <Navbar />
-      <Slider />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <UpperNavbar />
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Slider />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/partial/Navbar.jsx
+++ src/partial/Navbar.jsx
@@ -1,5 +1,5 @@
 import {PartialNavbar, PartialLink} from './Partial.style';
-
+import { Link } from 'react-router-dom'; 
 import SearchSharpIcon from '@mui/icons-material/SearchSharp';
 import PersonSharpIcon from '@mui/icons-material/PersonSharp';
 import FavoriteBorderSharpIcon from '@mui/icons-material/FavoriteBorderSharp';
@@ -8,15 +8,15 @@ import KeyboardArrowDownSharpIcon from '@mui/icons-material/KeyboardArrowDownSha
 const Navbar = () => {
   return (
     <PartialNavbar>
-      <img src="https://template.hasthemes.com/flone/flone/assets/img/logo/logo.png" />
+      <Link to="/"><img src="https://template.hasthemes.com/flone/flone/assets/img/logo/logo.png" /></Link>
       <PartialLink>
-        <a href="/">Home <KeyboardArrowDownSharpIcon sx={{ fontSize: 16 }}/></a>
-        <a href="/">Shop <KeyboardArrowDownSharpIcon sx={{ fontSize: 16 }}/></a>
-        <a href="/">Collection</a>
-        <a href="/">Pages <KeyboardArrowDownSharpIcon sx={{ fontSize: 16 }}/></a>
-        <a href="/">Blog <KeyboardArrowDownSharpIcon sx={{ fontSize: 16 }}/></a>
-        <a href="/">About</a>
-        <a href="/">Contact</a>
+        <a href="#">Home <KeyboardArrowDownSharpIcon sx={{ fontSize: 16 }}/></a>
+        <a href="#">Shop <KeyboardArrowDownSharpIcon sx={{ fontSize: 16 }}/></a>
+        <a href="#">Collection</a>
+        <a href="#">Pages <KeyboardArrowDownSharpIcon sx={{ fontSize: 16 }}/></a>
+        <a href="#">Blog <KeyboardArrowDownSharpIcon sx={{ fontSize: 16 }}/></a>
+        <a href="#">About</a>
+        <Link to="/contact">Contact</Link>
       </PartialLink>
       <PartialLink>
         <span><SearchSharpIcon /></span>
